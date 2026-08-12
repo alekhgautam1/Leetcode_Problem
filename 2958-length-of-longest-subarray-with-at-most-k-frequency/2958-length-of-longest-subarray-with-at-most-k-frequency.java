4db9991cd5373e85bc0e7143a043a35fd66c3303
ALEKH GAUTAM <alekhgautam3@gmail.com>
2026-08-12 21:27:00 +0530

    Time: 65 ms (85.94%), Space: 88.7 MB (74.81%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,26 +1,21 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class Solution {
+class Solution {
     public int maxSubarrayLength(int[] nums, int k) {
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        int maxLength = 0;
-        int left = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int left = 0, ans = 0;
 
         for (int right = 0; right < nums.length; right++) {
-  
-            frequencyMap.put(nums[right], frequencyMap.getOrDefault(nums[right], 0) + 1);
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
 
-           
-            while (frequencyMap.get(nums[right]) > k) {
-                frequencyMap.put(nums[left], frequencyMap.get(nums[left]) - 1);
+            while (map.get(nums[right]) > k) {
+                map.put(nums[left], map.get(nums[left]) - 1);
                 left++;
             }
 
-           
-            maxLength = Math.max(maxLength, right - left + 1);
+            ans = Math.max(ans, right - left + 1);
         }
 
-        return maxLength;
+        return ans;
     }
 }
